d12a0f17bd0077d9c90c0dc78ee19e536bdfb007
klebercperes <klebercperes.gmail.com>
2025-04-19 06:07:16 +0000

    After adding Ngnix

--- smdrproject/settings.py
+++ smdrproject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['43.247.67.119', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['peres.au', 'www.peres.au', 'localhost', '127.0.0.1', '10.0.1.104', '43.247.67.119']
                  
 
 # Application definition
